19ed0c7ea1e269c4a8c05e430e8851739958a3fc
Claire Quigley <claire.quigley@gmail.com>
2018-01-22 21:32:46 +0000

    Altered up symbol sound

--- makeymakey-demo.rb
+++ makeymakey-demo.rb
@@ -8,7 +8,7 @@
 up = "cue :upcue"
 down = "sample :misc_crow"
 left = "sample :perc_snap"
-right = "sample :misc_cineboom"
+right = "sample :ambi_choir"
 space = "use_synth :hoover \n play :c4"
 click = "play :g5"
 
@@ -18,25 +18,24 @@ sounds = [up, right, down, left, space, click]
 
 # loop triggered by pressing 'up' symbol
 live_loop :uploop do
-  sync :upcue
-  with_fx :echo do
-    sample :bass_trance_c
-    sleep 1
-    sample :bd_haus
-    sleep 0.5
-    sample :bd_haus
-    sleep 0.5
-  end
+    sync :upcue
+    with_fx :echo do
+        sample :bass_trance_c
+        sleep 1
+        sample :bd_haus
+        sleep 0.5
+        sample :bd_haus
+        sleep 0.5
+    end
 end
 
 
 # looks for messages from the MaKey MaKey via Pygame Zero
 # and runs appropriate command
 live_loop :get_note do
-  use_real_time
-  set_sched_ahead_time! 0     #removes 'lag' between pressing symbol and sound playing
-  n = sync "/osc/play"
-  symbol = n[0]
-  run_code sounds[pad]
+    use_real_time
+    set_sched_ahead_time! 0     #removes 'lag' between pressing symbol and sound playing
+    symbol = sync "/osc/play"
+    run_code sounds[symbol[0]]
 end
 
